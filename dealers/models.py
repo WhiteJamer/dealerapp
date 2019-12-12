b667c4e74e1f5f309921155e685dda1e394114a1
whitejamer <muxah3258@gmail.com>
2019-12-12 22:40:17 +0300

    Made dealer model name unique=True

--- dealers/models.py
+++ dealers/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from uprofile.models import User
+from django.shortcuts import reverse
 
 
 class Dealer(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     user = models.ForeignKey(User,
                              related_name='dealers',
                              on_delete=models.CASCADE, blank=True, null=True)
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('dealers:dealer_detail', kwargs={'slug':self.name})
