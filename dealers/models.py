5a3b3e10b357fea40fc1d65cd4d0bd3acf5826ba
whitejamer <muxah3258@gmail.com>
2019-12-12 20:52:51 +0300

    Make migration change field user blank=True null=True

--- dealers/models.py
+++ dealers/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from uprofile.models import User
 
 
 class Dealer(models.Model):
     name = models.CharField(max_length=255)
+    user = models.ForeignKey(User,
+                             related_name='dealers',
+                             on_delete=models.CASCADE, blank=True, null=True)
