6d6a36e219be7ce708fa2a7514257e49c75bc2d2
whitejamer <muxah3258@gmail.com>
2019-12-13 16:16:37 +0300

    Made uprofile simpler

--- uprofile/urls.py
+++ uprofile/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import url
-from .views import UserList, UserDetail, ProfileUpdate
+from .views import UserDetail
 
 app_name = 'uprofile'
 
 urlpatterns = [
-    url(r'^$', UserList.as_view(), name='user_list'),
-    url(r'^(?P<slug>[\w-]+)/$', UserDetail.as_view(), name='user_detail'),
-    url(r'^(?P<slug>[\w-]+)/edit/$', ProfileUpdate.as_view(), name='profile_update'),
+    url(r'^(?P<slug>[\w|\W]+)/$', UserDetail.as_view(), name='user_detail')
 ]
--- uprofile/views.py
+++ uprofile/views.py
@@ -1,12 +1,8 @@
 from django.views.generic import ListView, DetailView, View
 from .models import User
 
-class UserList(ListView):
-    model = User
-    queryset = User.objects.all()
-
-
 class UserDetail(DetailView):
+    slug_field = 'username'
     context_object_name = 'auser' # another user not request.user
     queryset = User.objects.all()
 
